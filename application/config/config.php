abf02895b8548dd8a34a081846beb8c41e5efee0
NathanHolanda <nathan.hl.contato@gmail.com>
2021-04-22 20:32:53 -0300

    personalização do input file em register parcialmente concluída

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+<<<<<<< Updated upstream
 $config['base_url'] = 'http://localhost/fauna';
+=======
+$config['base_url'] = 'http://localhost/fauna/';
+>>>>>>> Stashed changes
 
 /*
 |--------------------------------------------------------------------------
--- application/views/pages/Register.php
+++ application/views/pages/Register.php
@@ -90,17 +90,42 @@
         </form>
     </main>
     <footer>
-
     </footer>
 
     <script>
+        function readImage(file){
+            // Check if the file is an image.
+            if (file.type && !file.type.startsWith('image/')) {
+                console.log('File is not an image.', file.type, file);
+                return;
+            }
+
+            const reader = new FileReader();
+            reader.addEventListener('load', (event) => {
+                Array.from(document.querySelectorAll(".file-label")).forEach(el => {
+                    let div = el.querySelector("div");
+                    div.innerHTML = "";
+                    div.style.position = "relative";
+                    div.style.overflow = "hidden";
+
+                    div.appendChild(document.createElement("img"));
+                    div.querySelector("img").style.relative = "absolute";
+                    div.querySelector("img").style.width = "100%";
+                    div.querySelector("img").src = event.target.result;
+                });
+            });
+            reader.readAsDataURL(file);
+        }
+
         document.querySelector('#file-input1').addEventListener('input', e => {
             document.querySelector('#user-pic').textContent = e.target.files[0].name;
+            readImage(e.target.files[0]);
         });
         document.querySelector('#file-input2').addEventListener('input', e => {
             /*document.querySelector('#pet-pic').style.maxWidth = "150px";
             document.querySelector('#pet-pic').style.overflow = "hidden";*/
             document.querySelector('#pet-pic').textContent = e.target.files[0].name;
+            readImage(e.target.files[0]);
         });
     </script>
 </body>
