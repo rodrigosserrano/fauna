b4d6de3b5c5e641f0fd04dfe053004f7991db9a7
NathanHolanda <nathan.hl.contato@gmail.com>
2021-05-01 22:39:46 -0300

    alterações em common-feed.php

--- application/views/pages/common-feed.php
+++ application/views/pages/common-feed.php
@@ -16,12 +16,12 @@
                 
                 <div class="desc-n-menu">
                     <div class="desc">Legenda da foto</div>
-                    <div class="myDropdown-menu">
-                        <span class="myDropdown-icon"><i class="fas fa-ellipsis-v"></i></span>
-                        <div class="myDropdown-list">
+                    <div class="dropdown-post-menu">
+                        <span class="dropdown-post-icon"><i class="fas fa-ellipsis-v fa-xs"></i></span>
+                        <div class="dropdown-post-list">
                             <ul>
-                                <li id="edit-post" style="cursor:pointer;">Editar post</li>
-                                <li id="delete-post" style="cursor:pointer;">Apagar post</li>
+                                <li id="edit-post" style="cursor:pointer;">Editar postagem</li>
+                                <li id="delete-post" style="cursor:pointer;">Apagar postagem</li>
                             </ul>
                         </div>
                     </div>
@@ -29,7 +29,7 @@
                 
                 <div class="post-photo"></div>
                 <div class="post-likes">
-                    <div class="like-icon"></div>
+                    <div id="like-icon" style="background-image: url('<?= base_url()?>assets/img/icon/paw-like-unset.png')"></div>
                     <b><span id="likes-amount">55 pessoas curtiram isso</span></b>
                 </div>
                 <div class="comments">
@@ -37,22 +37,55 @@
                     <div class="comment">
                         <div class="comment-user-photo"></div> <!--Foto inserida com background-image-->
                         <div class="comment-content">
-                            <b><span class="comment-user">Nathan Holanda:</span></b>
-                            <span class="comment-text">Achei uma bosta</span>
+                            <div>
+                                <b><span class="comment-user">Nathan Holanda:</span></b>
+                                <span class="comment-text">Achei uma bosta</span>
+                            </div>
+                            <div class="dropdown-comment-menu">
+                                <span class="dropdown-comment-icon"><i class="fas fa-ellipsis-h fa-xs"></i></i></span>
+                                <div class="dropdown-comment-list">
+                                    <ul>
+                                        <li id="edit-comment" style="cursor:pointer;">Editar comentário</li>
+                                        <li id="delete-comment" style="cursor:pointer;">Apagar comentário</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="comment">
                         <div class="comment-user-photo"></div> <!--Foto inserida com background-image-->
                         <div class="comment-content">
-                            <b><span class="comment-user">Nathan Holanda:</span></b>
-                            <span class="comment-text">Achei uma bosta</span>
+                            <div>
+                                <b><span class="comment-user">Nathan Holanda:</span></b>
+                                <span class="comment-text">Achei uma bosta</span>
+                            </div>
+                            <div class="dropdown-comment-menu">
+                                <span class="dropdown-comment-icon"><i class="fas fa-ellipsis-h fa-xs"></i></i></span>
+                                <div class="dropdown-comment-list">
+                                    <ul>
+                                        <li id="edit-comment" style="cursor:pointer;">Editar comentário</li>
+                                        <li id="delete-comment" style="cursor:pointer;">Apagar comentário</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="comment">
                         <div class="comment-user-photo"></div> <!--Foto inserida com background-image-->
                         <div class="comment-content">
-                            <b><span class="comment-user">Nathan Holanda:</span></b>
-                            <span class="comment-text">Achei uma bosta</span>
+                            <div>
+                                <b><span class="comment-user">Nathan Holanda:</span></b>
+                                <span class="comment-text">Achei uma bosta</span>
+                            </div>
+                            <div class="dropdown-comment-menu">
+                                <span class="dropdown-comment-icon"><i class="fas fa-ellipsis-h fa-xs"></i></i></span>
+                                <div class="dropdown-comment-list">
+                                    <ul>
+                                        <li id="edit-comment" style="cursor:pointer;">Editar comentário</li>
+                                        <li id="delete-comment" style="cursor:pointer;">Apagar comentário</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -62,24 +95,24 @@
                 <div class="write-comments">
                     <form action="" method="post">
                         <textarea id="message">Escreva um comentário</textarea>
-                        <button id="send-comment" type="submit">➝</button>
+                        <button id="send-comment">➝</button>
                     </form>
                 </div>
             </div>
         </section>
         <section class="navigation">
             <div class="nav-container">
-                <div id="explore">
+                <div id="explore" class="nav-btns">
                     <span>Explorar</span>
                     <span>➝</span>
                 </div>
                 <div class="categories">
                     <h4 class="categories-title">Categoria de posts</h4>
-                    <div id="adoption">
+                    <div id="adoption" class="nav-btns">
                         <span>Adoção</span>
                         <span>➝</span>
                     </div>
-                    <div id="lost-animals">
+                    <div id="lost-animals" class="nav-btns">
                         <span>Animais perdidos</span>
                         <span>➝</span> 
                     </div>
@@ -88,7 +121,7 @@
             </div>
             <div class="copyright">
                 <span class="lang">Português (Brasil)</span>
-                <span class="copyright-desc">© FAUNA<?php echo date("Y");?></span>
+                <span class="copyright-desc">© FAUNA&nbsp<?php echo date("Y");?></span>
             </div>
         </section>
     </main>
@@ -102,6 +135,14 @@
     document.getElementById("message").addEventListener("blur", e => {
         if(e.target.value === "")
             e.target.value = "Escreva um comentário";
+    })
+
+    document.getElementById("like-icon").addEventListener("click", e => {
+        console.log(e.target.style.backgroundImage)
+        if(e.target.style.backgroundImage === "url('<?= base_url()?>assets/img/icon/paw-like-unset.png')")
+            e.target.style.backgroundImage = "url('<?= base_url()?>assets/img/icon/paw-like-set.png')"
+        else
+            e.target.style.backgroundImage = "url('<?= base_url()?>assets/img/icon/paw-like-unset.png')"
     })
 </script>
 </html>
