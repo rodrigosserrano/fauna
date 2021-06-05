ce151f13b57825dcf2f62de6b1adc226b1c5ced2
Seiki Nagaishi <seiki_nagaishi@hotmail.com>
2021-06-05 15:50:09 -0300

    Função Email

---
+++ application/controllers/EmailController.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class EmailController extends Fauna_Controller {
+
+    public function __construct() {
+        parent:: __construct();
+
+        $this->load->helper('url');
+    }
+
+    public function index() {
+        $this->load->view('email/contact');
+    }
+
+    function send($email, $senha) {
+        $this->load->config('email');
+        $this->load->library('email');
+        
+        $from = $this->config->item('smtp_user');
+        $to = $email;
+        $subject = 'Solicitação de alteração de senha';
+        $message = 'Sua senha agora é' + $senha;
+
+        $this->email->set_newline("\r\n");
+        $this->email->from($from);
+        $this->email->to($to);
+        $this->email->subject($subject);
+        $this->email->message($message);
+
+        if ($this->email->send()) {
+            echo 'Seu Email foi enviado com sucesso.';
+        } else {
+            show_error($this->email->print_debugger());
+        }
+    }
+
+}
