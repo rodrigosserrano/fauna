ff6ab2727d5c1453c9d0f4a27bb871a5888589b4
João Victor Araujo Bruzetti <50773970+JoaoBruzetti@users.noreply.github.com>
2021-06-06 03:30:43 -0300

    Models de  Seguir e Curtida

--- application/models/CurtidaModel.php
+++ application/models/CurtidaModel.php
@@ -1,55 +1,42 @@
 <?php
 
-class ComentarioModel extends CI_Model {
+class CurtidaModel extends CI_Model {
 
-    public function cadastroComentarioModel(array $dados_comentario){
-        if($this->db->insert('comentario', $dados_comentario))
+    public function cadastroCurtidaModel($id_usuario, $id_postagem  = null,  $id_comentario = null){
+
+        $dados_curtida['id_usuario'] = $id_usuario;
+        $dados_curtida['id_postagem'] = $id_postagem;
+        $dados_curtida['id_comentario'] = $id_comentario;
+
+        if($this->db->insert('curtida', $dados_curtida))
             return true;
     }
 
-    public function alterarDadosComentarioModel($dados_update, $nivel_usuario){
-        extract($dados_update);
+    public function deleteCurtidaModel($id_curtida){
 
-        $dados = [
-            'texto' => $texto
-            //'dh_comentario' => $dh_comentario
-            /*
-                'tipo' => $tipo,
-                'raca' =>  $raca_animal,
-                'sexo_animal' => $sexo_animal
-            */
-        ];
-        
-        $this->db->where('id_comentario', $id_comentario);
-
-        // Não for admin
-        if($nivel_usuario != 1) {
-            $this->db->where('id_usuario', $id_usuario);
-        }
-        
-        return $this->db->update('comentario', $dados);
+        return $this->db->where('id_curtida', $id_curtida)->delete('curtida');
     }
 
-    public function deleteComentarioModel($id_comentario, $id_usuario, $nivel_usuario){
-        if($nivel_usuario == 1) {
-            return $this->db->where('id_comentario', $id_comentario)->delete('comentario');
-        }
-
-        return $this->db->where('id_comentario', $id_comentario)->where('id_usuario', $id_usuario)->delete('comentario');
-    }
-
-    public function getDadosComentarioModel($id_postagem){
+    public function countCurtidaModel($id_postagem=null, $id_comentario=null){
         //$query = $this->db->where(['id_postagem' => $id_postagem])->get('comentario');
+        if($id_postagem){
+
+            $this->db->count_all_results('curtida')->where('id_postagem', $id_postagem);
+            $query = $this->db->get(); 
+            return $query->result();
+
+        }else{
+            if($id_comentario){
+
+                $this->db->count_all_results('curtida')->where('id_comentario', $id_comentario);
+                $query = $this->db->get(); 
+                return $query->result();
+
+            }else{
+                return false; 
+            }
+        }
         
-        
-        $this->db->select('u.id_usuario, c.id_comentario, u.nome_usuario as usuario, u.foto_usuario, u.email, c.texto, c.dh_comentario');
-        $this->db->from('comentario c'); 
-        $this->db->join('postagem p', 'c.id_postagem = p.id_postagem', 'left');
-        $this->db->join('usuario u', 'u.id_usuario = c.id_usuario', 'left');
-        $this->db->where('p.id_postagem = '. $id_postagem);
-        $this->db->order_by('c.dh_comentario','desc');
-        $query = $this->db->get(); 
-        return $query->result();
     }
 
 }
---
+++ application/models/SeguirModel.php
@@ -0,0 +1,80 @@
+<?php
+
+class SeguirModel extends CI_Model {
+
+    public function cadastroSeguirModel($id_usuario,  $id_usuario_seguido){
+
+        $dados_seguir['id_seguido'] = $id_usuario_seguido;
+        $dados_seguir['id_seguidor'] = $id_usuario;
+
+        if($this->db->insert('seguir', $dados_seguir))
+            return true;
+    }
+
+    public function deleteSeguirModel($id_usuario, $id_seguido = null, $id_seguidor = null){
+
+        if($id_seguido) {
+            return $this->db->where('id_seguidor', $id_usuario)->where('id_seguido', $id_seguido)->delete('seguir');
+        }else{
+            if($id_seguidor){
+                return $this->db->where('id_seguido', $id_usuario)->where('id_seguidor', $id_seguidor)->delete('seguir');
+            }else{
+                return false;
+            }
+        }
+       
+    }
+
+    public function getDadosSeguirModel($id_usuario, $seguindo = null, $seguidores = null){
+        //$query = $this->db->where(['id_postagem' => $id_postagem])->get('comentario');
+        
+        if($seguindo){
+
+            $this->db->select('u.id_usuario, u.nome_usuario as usuario, u.foto_usuario, u.email');
+            $this->db->from('seguir s'); 
+            $this->db->join('usuario u', 'u.id_usuario = c.id_seguido', 'left');
+            $this->db->where('s.id_seguidor = '. $id_usuario);
+            $this->db->order_by('u.nome_usuario','asc');
+            $query = $this->db->get(); 
+            return $query->result();
+
+        }else{
+            if($seguidores){
+                
+            $this->db->select('u.id_usuario, u.nome_usuario as usuario, u.foto_usuario, u.email');
+            $this->db->from('seguir s'); 
+            $this->db->join('usuario u', 'u.id_usuario = c.id_seguidor', 'left');
+            $this->db->where('s.id_seguido = '. $id_usuario);
+            $this->db->order_by('u.nome_usuario','asc');
+            $query = $this->db->get(); 
+            return $query->result();
+
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public function countSeguirModel($id_usuario, $seguindo = null, $seguidores = null){
+        //$query = $this->db->where(['id_postagem' => $id_postagem])->get('comentario');
+        if($seguindo){
+
+            $this->db->count_all_results('seguir')->where('id_seguidor', $id_usuario);
+            $query = $this->db->get(); 
+            return $query->result();
+
+        }else{
+            if($seguidores){
+
+                $this->db->count_all_results('seguir')->where('id_seguido', $id_usuario);
+                $query = $this->db->get(); 
+                return $query->result();
+
+            }else{
+                return false; 
+            }
+        }
+        
+    }
+
+}
